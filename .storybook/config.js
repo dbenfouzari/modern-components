0250f4f1f95531494d72d43a6267f6f9059cab08
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2018-11-30 12:05:26 +0100

    Added addon-info to show props

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { withInfo } from "@storybook/addon-info";
+
+addDecorator(withInfo);
 
 const req = require.context('../src', true, /.stories.tsx$/);
 

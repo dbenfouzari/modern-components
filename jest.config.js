c9b7839d1e8793cda8275b7bb8b1f774b78f02b4
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2018-11-30 12:16:09 +0100

    Added really basic and dumb test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/lib/"],
+  // testRegex: "(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+};
